c19baf389c2b4134f7662215800f174b7671b964
lf-ramesh <rameshlf786@gmail.com>
2022-10-17 15:24:44 +0530

    Updates

---
+++ precache-manifest.bd70091f03bee5531a6bd8deb50e14d1.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0160a008c56786261bb4",
+    "url": "/getcoolthemes/static/css/main.7a785038.chunk.css"
+  },
+  {
+    "revision": "0160a008c56786261bb4",
+    "url": "/getcoolthemes/static/js/main.0160a008.chunk.js"
+  },
+  {
+    "revision": "f12bf432d1b1b25fb939",
+    "url": "/getcoolthemes/static/js/1.f12bf432.chunk.js"
+  },
+  {
+    "revision": "c9669c3178700a51b675",
+    "url": "/getcoolthemes/static/js/2.c9669c31.chunk.js"
+  },
+  {
+    "revision": "14e4afcecd48c95c6863",
+    "url": "/getcoolthemes/static/js/runtime~main.14e4afce.js"
+  },
+  {
+    "revision": "6c027773a563cbd040364fa6b10fbc94",
+    "url": "/getcoolthemes/static/media/getcoolthemes-img.6c027773.png"
+  },
+  {
+    "revision": "816d9b4ab2b1aee8251aefd020d2fb48",
+    "url": "/getcoolthemes/static/media/uay-img.816d9b4a.jpg"
+  },
+  {
+    "revision": "d8ca70b8ed1c0e841bdcbbd16cd2c06b",
+    "url": "/getcoolthemes/static/media/signstamper-img.d8ca70b8.jpg"
+  },
+  {
+    "revision": "dedad64e9b5c64a6ba7ac4f9480326f3",
+    "url": "/getcoolthemes/static/media/rvsaircon-img.dedad64e.jpg"
+  },
+  {
+    "revision": "5da28a3735eebda2d57832165c0ddf0f",
+    "url": "/getcoolthemes/static/media/saamosa-img.5da28a37.jpg"
+  },
+  {
+    "revision": "55080c1ad24e01d3231ce39d41308af5",
+    "url": "/getcoolthemes/static/media/crr-img.55080c1a.jpg"
+  },
+  {
+    "revision": "8da3a12e27a7e9d6fc87b0738d07b898",
+    "url": "/getcoolthemes/static/media/ers-img.8da3a12e.jpg"
+  },
+  {
+    "revision": "da87e513417bb66fbb3a677386818f33",
+    "url": "/getcoolthemes/static/media/tymey-img.da87e513.jpg"
+  },
+  {
+    "revision": "478f44eb2b1d5d5e43d241ef1ed177ef",
+    "url": "/getcoolthemes/static/media/emcservices-img.478f44eb.jpg"
+  },
+  {
+    "revision": "7fc42b5111447497fd61abb017ec0383",
+    "url": "/getcoolthemes/index.html"
+  }
+];
